e0852cfdffce97f183b990a3fc33953ceff6e030
Regal Zhang <nescirem@gmail.com>
2021-07-31 12:08:28 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,41 +1,48 @@
 # name: discourse-onebox-bilibili
 # about: 为 Discourse Onebox 增加了 bilibili 视频支持
-# version: 0.0.1
+# version: 0.0.2
 # authors: MuZhou233
 # url: https://github.com/MuZhou233/discourse-onebox-bilibili
 
-class Onebox::Engine::BilibiliOnebox
-  include Onebox::Engine
+# modified by nescirem
+# url: https://github.com/nescirem/discourse-onebox-bilibili 
 
-  matches_regexp(/^https?:\/\/(?:www\.)?bilibili\.com\/video\/([a-zA-Z0-9]+)\/?$/)
-  always_https
+Onebox = Onebox
 
-  def video_id
-    match = uri.path.match(/\/video\/av(\d+)(\.html)?.*/)
-    return "aid=#{match[1]}" if match && match[1]
-    match = uri.path.match(/\/video\/BV([a-zA-Z0-9]+)(\.html)?.*/)
-    return "bvid=#{match[1]}" if match && match[1]
-      
-    nil
-  rescue
-    return nil
-  end
 
-  def to_html
-    <<-HTML
-      <iframe 
-        src='https://player.bilibili.com/player.html?#{video_id}&page=1' 
-        scrolling="no" 
-        border="0" 
-        frameborder="no" 
-        framespacing="0" 
-        width='640' 
-        height='430' 
-        allowfullscreen='true'></iframe>
-    HTML
-  end
+module Onebox
+   module Engine
+     class BilibiliOnebox
+       include Engine
+       include HTML
+       
+       REGEX = /^https?:\/\/(?:www\.)?bilibili\.com\/video\/([a-zA-Z0-9]+)\/?$/
+       matches_regexp REGEX
 
-  def placeholder_html
-    to_html
-  end
+       def video_id
+         match = uri.path.match(/\/video\/av(\d+)\?.*/)
+         return "aid=#{match[1]}" if match && match[1]
+         match = uri.path.match(/\/video\/BV([a-zA-Z0-9]+)\?.*/)
+         return "bvid=#{match[1]}" if match && match[1]
+
+         nil
+       rescue
+         return nil
+       end
+
+       def to_html
+         <<-HTML
+           <iframe 
+             src='https://player.bilibili.com/player.html?#{video_id}&page=1' 
+             scrolling="no" 
+             border="0" 
+             frameborder="no" 
+             framespacing="0" 
+             width='640' 
+             height='430' 
+             allowfullscreen='true'></iframe>
+         HTML
+       end
+     end
+   end
 end
